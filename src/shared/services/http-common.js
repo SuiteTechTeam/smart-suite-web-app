0a9a1105e2a146d56d480f8470d4deff7ccd1bca
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2025-05-12 21:41:55 -0500

    fix(http): update base URL configuration to use environment variable

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 axios.defaults.withCredentials = true;
 
 const http = axios.create({
-    baseURL: 'https://sweetmanager.ryzeon.me/',
+    baseURL: API_BASE_URL,
     headers: { 'content-type': 'application/json'}
 });
 
